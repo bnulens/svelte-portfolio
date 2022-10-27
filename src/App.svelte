a1a3ea6a2467209a726bccba0ac3505718fbe219
bnulens <brechtnulens@gmail.com>
2022-10-27 13:53:16 +0200

    Inserted component sections into App

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,21 @@
 <script>
-  console.log("Root");
+  import Summary from "./components/Summary.svelte";
+  import Hero from "./components/Hero.svelte";
+  import Profile from "./components/Profile.svelte";
+  import Academics from "./components/Academics.svelte";
+  import Projects from "./components/Projects.svelte";
+  import Footer from "./components/Footer.svelte";
+  import Career from "./components/Career.svelte";
 </script>
 
 <div class="app-inner-wrapper">
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
-  <p>A</p>
+  <div class="app-bootstrap">
+    <Hero />
+    <Profile />
+    <Projects />
+    <Career />
+    <Academics />
+    <Footer />
+  </div>
+  <Summary />
 </div>
