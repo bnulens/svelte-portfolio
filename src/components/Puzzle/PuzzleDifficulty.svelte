4ed8e6f7428feacfde6bd0ffaf017cd528d12dc8
bnulens <brechtnulens@gmail.com>
2022-11-23 01:21:14 +0100

    feat - Drop-down selector for Puzzle size

---
+++ src/components/Puzzle/PuzzleDifficulty.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let value;
+  export let buildGrid = () => {};
+
+  const eraseGrid = () => {
+    let puzzleGrid = document.getElementById("puzzle-grid");
+    puzzleGrid.replaceChildren();
+  };
+
+  const selectSize = (e) => {
+    eraseGrid();
+    value = parseInt(e.target.value);
+    buildGrid(e.target.value);
+  };
+
+  const [small, medium, large, huge] = [9, 16, 25, 36];
+</script>
+
+<div class="c-puzzle-difficulty-selector">
+  <select bind:value>
+    <option value={small} on:click={selectSize}>Small</option>
+    <option value={medium} on:click={selectSize}>Medium</option>
+    <option value={large} on:click={selectSize}>Large</option>
+    <option value={huge} on:click={selectSize}>Huge</option>
+  </select>
+</div>
