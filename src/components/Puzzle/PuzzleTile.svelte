a2176325a8d52b706b03132f416ffeb51eac9324
bnulens <brechtnulens@gmail.com>
2022-12-15 16:43:29 +0100

    feat - Create separate puzzle tile component

---
+++ src/components/Puzzle/PuzzleTile.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let puzzleSizeString;
+  export let puzzleSizeValue;
+  export let tileValue;
+</script>
+
+<div class="c-puzzle-tile">
+  {#if tileValue === puzzleSizeValue - 1}
+    <span />
+  {:else}
+    <img
+      class="o-puzzle-tile-image"
+      src={`src/assets/images/puzzle/${puzzleSizeString}/${tileValue}.jpg`}
+      alt="puzzle-piece"
+    />
+  {/if}
+</div>
