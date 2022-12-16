97bb6480dccabbf230c43781eb69e32c600df413
bnulens <brechtnulens@gmail.com>
2022-12-17 00:47:51 +0100

    feat - button with dynamic image path & function

---
+++ src/components/ToggleViewMode.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { toggleViewMode as viewMode } from "../scripts/pageStore";
+
+  const toggle = () => viewMode.set(!$viewMode);
+</script>
+
+<div class="c-page-btn wrapper">
+  <button
+    class="o-btn small {$viewMode ? 'is-dark' : 'is-light'}"
+    on:click={toggle}
+  >
+    <img
+      class="o-btn-icon"
+      src="src/assets/icons/{$viewMode ? 'light' : 'dark'}/{$viewMode
+        ? 'sun-light'
+        : 'moon-dark'}.svg"
+      alt="view-mode-icon"
+    />
+  </button>
+</div>
