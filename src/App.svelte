cad4a77afe7362d01d8f42bf4f0b59c26c90d3d3
bnulens <brechtnulens@gmail.com>
2022-12-17 00:45:41 +0100

    feat - Add generic ToggleViewMode button

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
   import Summary from "./components/Summary.svelte";
-  import Hero from "./components/Hero.svelte";
-  import Profile from "./components/Profile.svelte";
-  import Academics from "./components/Academics.svelte";
-  import Projects from "./components/Projects.svelte";
-  import Footer from "./components/Footer.svelte";
-  import Career from "./components/Career.svelte";
+  import Hero from "./components/Sections/Hero.svelte";
+  import Profile from "./components/Sections/Profile.svelte";
+  import Academics from "./components/Sections/Academics.svelte";
+  import Projects from "./components/Sections/Projects.svelte";
+  import Footer from "./components/Sections/Footer.svelte";
+  import Career from "./components/Sections/Career.svelte";
+  import ToggleViewMode from "./components/ToggleViewMode.svelte";
 </script>
 
 <div class="app-inner-wrapper">
+  <ToggleViewMode />
   <div class="app-bootstrap">
     <Hero />
     <Profile />
