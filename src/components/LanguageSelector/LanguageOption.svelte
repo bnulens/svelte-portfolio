f6299eb1bc835b14a1583dbc4438be029ffbb452
bnulens <brechtnulens@gmail.com>
2022-12-20 18:08:14 +0100

    feat - Separate LanguageSelector folder

---
+++ src/components/LanguageSelector/LanguageOption.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let lang;
+  import { pageSettings } from "../../scripts/pageStore";
+
+  const selectLanguage = (lang) => {
+    pageSettings.toggleLanguage.set();
+  };
+</script>
+
+<div class="c-lang-option wrapper">
+  <li class="o-lang-option">
+    <img src="src/assets/icons/countries/${lang}.svg" alt="country-icon" />
+  </li>
+</div>
---
+++ src/components/LanguageSelector/LanguageSelector.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { pageSettings } from "../../scripts/pageStore";
+
+  import LanguageOption from "./LanguageOption.svelte";
+
+  let alllanguages = ["en", "es", "fr", "nl"];
+  let indexOfSelected = alllanguages.indexOf($pageSettings.toggleLanguage);
+  let otherLanguages = alllanguages.splice(alllanguages[indexOfSelected], 1);
+
+  console.log($pageSettings.toggleLanguage);
+</script>
+
+<div class="c-lang-selector">
+  <img src="/src/assets/" alt="" />
+  {$pageSettings.toggleLanguage}
+  <ul class="c-lang-selector lang-list">
+    {#each otherLanguages as lang}
+      <LanguageOption {lang} />
+    {/each}
+  </ul>
+</div>
