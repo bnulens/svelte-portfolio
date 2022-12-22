13c8b54390a1c6789018f57d4afeb94b94d8959d
bnulens <brechtnulens@gmail.com>
2022-12-22 14:27:45 +0100

    refactor - pageStore is a writable object

--- src/scripts/pageStore.js
+++ src/scripts/pageStore.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
 
-export const toggleResume = writable(false);
-export const toggleViewMode = writable(false);
-export const toggleLanguage = writable("en");
+export const pageSettings = writable({
+  toggleResume: false,
+  toggleViewMode: false,
+  preferredLanguage: "en",
+});
