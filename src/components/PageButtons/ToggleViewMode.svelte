c49c6cdf4dd2a9ed97bdf92477f4952b14827bbd
bnulens <brechtnulens@gmail.com>
2022-12-28 00:37:15 +0100

    chore - Move ToggleViewMode to separate buttons folder

---
+++ src/components/PageButtons/ToggleViewMode.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { settings } from "../../scripts/pageStore";
+
+  const toggle = () => settings.toggleViewMode(!$settings.isDarkMode);
+</script>
+
+<div class="c-page-btn wrapper">
+  <button
+    class="o-btn small {$settings.isDarkMode ? 'is-dark' : 'is-light'}"
+    on:click={toggle}
+  >
+    <img
+      class="o-btn-icon"
+      src="src/assets/icons/{$settings.isDarkMode
+        ? 'light'
+        : 'dark'}/{$settings.isDarkMode ? 'sun-light' : 'moon-dark'}.svg"
+      alt="view-mode-icon"
+    />
+  </button>
+</div>
