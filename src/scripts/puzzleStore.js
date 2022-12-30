fa9c1de9ac36962120491625f261d9238f58ec72
bnulens <brechtnulens@gmail.com>
2022-12-30 15:56:10 +0100

    refactor - For loop generate unique number array

--- src/scripts/puzzleStore.js
+++ src/scripts/puzzleStore.js
@@ -7,7 +7,11 @@ export const isPlayingPuzzle = writable(false);
 export const isPuzzleSolved = writable(false);
 
 export const generateGridArray = (gridSize) => {
-  const _solvedArray = [...Array(gridSize).keys()];
+  const _solvedArray = [];
+  for (let i = 1; i <= gridSize; i++) {
+    _solvedArray.push(i);
+  }
+
   solvedArray.set(JSON.stringify(_solvedArray));
   const shuffledArray = _solvedArray.sort(() => Math.random() - 0.5);
   puzzleArray.set(shuffledArray);
