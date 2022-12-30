2802414be4d3f8887f53a1db674fc8e46f610874
bnulens <brechtnulens@gmail.com>
2022-12-30 15:54:46 +0100

    feat - Language writable is nested object - Add toggle methods

--- src/scripts/pageStore.js
+++ src/scripts/pageStore.js
@@ -1,23 +1,41 @@
 import { writable } from "svelte/store";
 
 let languages = "en,nl,fr,es";
-export const availableLanguages = languages.split(",");
+export const allLanguages = languages.split(",");
 
 const pageSettings = () => {
   const { subscribe, set, update } = writable({
     isResumeExpanded: false,
     isDarkMode: false,
-    isLanguageExpanded: false,
-    preferredLanguage: "en",
+    language: {
+      preferredLanguage: "en",
+      isSelectorExpanded: false,
+    },
   });
 
   return {
     subscribe,
     set,
     update,
+
+    toggleLangSelector: (val) =>
+      update((self) => {
+        self.language.isSelectorExpanded = val;
+        return self;
+      }),
+    toggleResume: (val) =>
+      update((self) => {
+        self.isResumeExpanded = val;
+        return self;
+      }),
     toggleViewMode: (val) =>
       update((self) => {
         self.isDarkMode = val;
+        return self;
+      }),
+    selectLanguage: (val) =>
+      update((self) => {
+        self.language.preferredLanguage = val;
         return self;
       }),
   };
