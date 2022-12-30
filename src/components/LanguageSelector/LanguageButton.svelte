73d5cbd3441c6296acaa88fb6a0df97e42d0a600
bnulens <brechtnulens@gmail.com>
2022-12-30 15:52:51 +0100

    feat - Expandable language selector

---
+++ src/components/LanguageSelector/LanguageButton.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { settings, allLanguages } from "../../scripts/pageStore";
+  import LanguageSelector from "./LanguageSelector.svelte";
+
+  $: isSelectorExpanded = $settings.language.isSelectorExpanded;
+
+  const toggleSelectorList = () => {
+    settings.toggleLangSelector(!isSelectorExpanded);
+  };
+</script>
+
+<div class="c-language-selector-button">
+  <div class="c-language-selector-button wrapper">
+    <button class="o-language-selector-button" on:click={toggleSelectorList}
+      ><img
+        class="o-language-selector icon"
+        src="src/assets/icons/countries/{$settings.language
+          .preferredLanguage}.svg"
+        alt="country-flag"
+      />
+      <p class="btn-text sm">
+        {$settings.language.preferredLanguage}
+      </p>
+    </button>
+  </div>
+  {#if isSelectorExpanded}
+    <LanguageSelector {allLanguages} />
+  {/if}
+</div>
