133dde29db29be2664853adc83fa547de6c10025
bnulens <brechtnulens@gmail.com>
2022-12-30 16:04:49 +0100

    feat - Create language option list

--- src/components/LanguageSelector/LanguageSelector.svelte
+++ src/components/LanguageSelector/LanguageSelector.svelte
@@ -1,20 +1,11 @@
 <script>
-  import { pageSettings } from "../../scripts/pageStore";
-
+  export let allLanguages;
   import LanguageOption from "./LanguageOption.svelte";
-
-  let alllanguages = ["en", "es", "fr", "nl"];
-  let indexOfSelected = alllanguages.indexOf($pageSettings.toggleLanguage);
-  let otherLanguages = alllanguages.splice(alllanguages[indexOfSelected], 1);
-
-  console.log($pageSettings.toggleLanguage);
 </script>
 
 <div class="c-lang-selector">
-  <img src="/src/assets/" alt="" />
-  {$pageSettings.toggleLanguage}
-  <ul class="c-lang-selector lang-list">
-    {#each otherLanguages as lang}
+  <ul class="o-lang-selector lang-list">
+    {#each allLanguages as lang}
       <LanguageOption {lang} />
     {/each}
   </ul>
