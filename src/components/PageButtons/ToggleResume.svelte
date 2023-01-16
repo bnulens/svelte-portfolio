4e26eebc8f010a322797534226a66b11ecadafe5
bnulens <brechtnulens@gmail.com>
2023-01-16 02:01:08 +0100

    feat - Delegate toggle resume to separate button

---
+++ src/components/PageButtons/ToggleResume.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { t } from "../../i18n/i18n";
+  import { settings } from "../../scripts/pageStore";
+
+  $: isResumeExpanded = $settings.isResumeExpanded;
+
+  const toggleResume = () => {
+    settings.toggleResume(!isResumeExpanded);
+    return;
+  };
+</script>
+
+<div class="o-toggle-resume">
+  <button class="e-toggle" on:click={toggleResume}
+    >{$t("utils.buttons.skip")}</button
+  >
+</div>
